768e086c502ed80ff6b87d25dd012d22f7fa6e07
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-09-14 20:59:23 +0100

    Added Collection component

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,23 @@ import React from "react";
 import Hero from "@/components/Hero";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
-import Products from "@/components/Products"
+import Products from "@/components/Products";
+import Collection from "@/components/Collection";
 import { stringify } from "postcss";
 
-export default function Home({featuredProduct, newProducts}) {
+export default function Home({featuredProduct, newProducts, collectionProduct}) {
   return (
     <>
       <div className="">
         <Hero product={featuredProduct} />
-        
+
         <hr class="my-8 h-px border-0 bg-gray-300" />
 
-        <Products products={newProducts}/>
+        <Products products={newProducts} />
+
+        <hr class="my-8 h-px border-0 bg-gray-300" />
+
+        <Collection product={collectionProduct} />
       </div>
     </>
   );
@@ -22,21 +27,23 @@ export default function Home({featuredProduct, newProducts}) {
 export async function getServerSideProps(){
   await mongooseConnect();
 
-  const featuredId = "66e27286ca2a680f0c097c01";
-  const collectionId = "66e480df1c0d4addb350cda3";
+  const featuredProduct = await Product.findOne({}, null, {
+    sort: { _id: 1 },
+  });
 
-  const featuredProduct = await Product.findById(featuredId);
   const newProducts = await Product.find({}, null, {
     sort: { _id: 1 },
     limit: 5,
   });
-  const collectionProduct = await Product.findById(collectionId);
+  const collectionProduct = await Product.findOne({}, null, {
+    sort: { _id: -1 },
+  });
 
   return {
     props: {
       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
       newProducts: JSON.parse(JSON.stringify(newProducts)),
-      collectionProduct: JSON.parse(stringify(collectionProduct)),
+      collectionProduct: JSON.parse(JSON.stringify(collectionProduct)),
     },
   };
 }
