1a1362cf9b11bd940b8216983a47bf167f388ae1
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-09-18 18:30:02 +0100

    Added more functionalities to the header

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,29 @@
 import { CartContext } from "@/lib/CartContext";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 
-export default function Header() {
+function Header() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const [currentPath, setCurrentPath] = useState("");
   const router = useRouter();
-
   const { pathname } = router;
-
   const {cartProducts} = useContext(CartContext)
+  const { data:session } = useSession();
+
+  useEffect(() => {
+    setCurrentPath(window.location.pathname)
+  }, [])
+
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
 
   const active = "text-primary transition hover:text-secondary-500 font-bold";
   const inactive = "text-gray-500 transition hover:text-gray-500/75";
+
   return (
     <>
       <header className="bg-white border-b border-primary border-opacity-40 sticky top-0 z-40">
@@ -65,30 +76,58 @@ export default function Header() {
             </nav>
 
             <div className="flex items-center gap-4">
-              <div className="sm:flex sm:gap-4">
-                <Link
-                  className="px-5 py-2.5 text-sm text-center font-medium transition border-r border-primary"
-                  href="/"
-                >
-                  Account
-                </Link>
-
-                <Link
-                  className="hidden px-5 py-2.5 rounded-md text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block"
-                  href="/cart"
-                >
+              {session ? (
+                <div className="sm:flex sm:gap-2 border-r border-primary pr-4">
+                  <div className="h-9 w-9">
+                    <img
+                      className="h-full w-full rounded-full object-cover object-center"
+                      src={session.user.image}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <div className="sm:flex sm:gap-2 border-r border-primary pr-4">
+                  <Link
+                    className="text-md font-medium text-text hidden md:flex"
+                    href="/"
+                  >
+                    Account
+                  </Link>
+                  <Link
+                    className="text-md font-medium text-text hidden max-md:flex md:hidden"
+                    href="/"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                  </Link>
+                </div>
+              )}
+              <div className="ml-4 flow-root lg:ml-4">
+                <Link href="/cart" className="group -m-2 flex items-center p-s">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
                     stroke="currentColor"
-                    className="size-6"
+                    class="w-6 h-6"
                   >
                     <path
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                      d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
                     />
                   </svg>
                   <span className="ml-2 text-md text-primary font-bold group-hover:text-text">
@@ -98,27 +137,147 @@ export default function Header() {
                 </Link>
               </div>
 
-              <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
-                <span className="sr-only">Toggle menu</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
+              <div className="block mr-0 md:hidden">
+                <button
+                  onClick={toggleMobileNav}
+                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
+                  {isMobileNavOpen ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+
+              {isMobileNavOpen && (
+                <div className="md:hidden absolute top-24 right-0 bg-white border border-zinc-200 rounded shadow-lg p-6 text-lg">
+                  <nav aria-label="Global">
+                    <ul className="flex flex-col items-start gap-6 text-md">
+                      <li>
+                        <Link
+                          className={pathname === "/" ? active : inactive}
+                          href="/"
+                          onClick={toggleMobileNav}
+                        >
+                          Home
+                        </Link>
+                      </li>
+
+                      <li>
+                        <Link
+                          className={
+                            pathname === "/products" ? active : inactive
+                          }
+                          href="/products"
+                          onClick={toggleMobileNav}
+                        >
+                          All Products
+                        </Link>
+                      </li>
+                      <li>
+                        {session && (
+                          <button onClick={() => signOut()}>logout</button>
+                        )}
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              )}
             </div>
           </div>
+        </div>
+      </header>
+
+      <header className="md:hidden w-full flex justify-around items-center my-3 border-b fixed top-12 bg-gray-200 z-50">
+        <div className="inline-flex gap-8 p-1">
+          <Link href="/" className={pathname === "/" ? active : inactive}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-4 h-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+            Home
+          </Link>
+          <Link
+            href="/products"
+            className={pathname === "/products" ? active : inactive}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-4 h-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+              />
+            </svg>
+            Products
+          </Link>
+          <Link
+            href="/cart"
+            className={pathname === "/cart" ? active : inactive}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-4 h-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+            Cart
+          </Link>
         </div>
       </header>
     </>
   );
 }
+
+export default Header;
